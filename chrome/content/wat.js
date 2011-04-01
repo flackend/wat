ed8681fbcd7faef2a47ef2489fb208aca2710c18
teramako <teramako@gmail.com>
2011-04-01 22:03:33 +0900

    remove non-used variable

--- chrome/content/wat.js
+++ chrome/content/wat.js
@@ -44,7 +44,7 @@ let WAT = (function(){
   const searchService = Cc["@mozilla.org/browser/search-service;1"].getService(Ci.nsIBrowserSearchService);
   const WAT_FORWARD_CMD = "wat_cmd_browserGoForward",
         WAT_BACK_CMD    = "wat_cmd_browserGoBack";
-  let popupElm = null, bundle = null;
+  let bundle = null;
 
   /**
    * Support browser forward and back.
@@ -103,7 +103,6 @@ let WAT = (function(){
   function init(){
     window.removeEventListener("load", init, false);
     self.tabMail = $("tabmail");
-    popupElm = $("wat_menuPopup");
     bundle = $("bundle_wat");
     migrateBookmarks();
 
