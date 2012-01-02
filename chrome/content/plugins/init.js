d5ce06765465cf0880fa4ba626641d91e089896f
teramako <teramako@gmail.com>
2012-01-02 19:51:08 +0900

    remove obsolete function and should load if extension version is same.

--- chrome/content/plugins/init.js
+++ chrome/content/plugins/init.js
@@ -19,27 +19,13 @@ function load(url, scriptObj){
   contexts[name] = context;
   Services.scriptloader.loadSubScript(url, context);
 }
-function isExtensionEnabled(extItem){
-  const rdf = Cc["@mozilla.org/rdf/rdf-service;1"].getService(Ci.nsIRDFService),
-        PREFIX_ITEM_URI = "urn:mozilla:item:",
-        PREFIX_NS_EM = "http://www.mozilla.org/2004/em-rdf#";
-  let enabled = false;
-  let item = rdf.GetResource(PREFIX_ITEM_URI + extItem.id);
-  if (item){
-    let target = extManager.datasource.GetTarget(item, rdf.GetResource(PREFIX_NS_EM + "isDisabled"), true);
-    if (target && target instanceof Ci.nsIRDFLiteral){
-      enabled = (target.Value != "true");
-    }
-  }
-  return enabled;
-}
 (function(){
   const PRE_PATH = "chrome://wat/content/plugins/";
   for (let i=0, len=scripts.length; i<len; i++){
     let s = scripts[i];
     let url = PRE_PATH + s.FILENAME;
     AddonManager.getAddonByID(s.EXTENSION_ID, function (ext) {
-      if (ext && ext.isActive && Services.vc.compare(s.VERSION, ext.version) < 0) {
+      if (ext && ext.isActive && Services.vc.compare(s.VERSION, ext.version) <= 0) {
         load(url, s);
       }
     });
