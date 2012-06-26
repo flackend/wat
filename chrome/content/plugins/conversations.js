40bc6c9a87e944651b3ece9509f708e6331cdab0
teramako <teramako@gmail.com>
2012-06-26 22:15:42 +0900

    fix bug: context menu doesnot show on multimessageview for Conversations

--- chrome/content/plugins/conversations.js
+++ chrome/content/plugins/conversations.js
@@ -14,3 +14,27 @@ function onClick (aEvent) {
   }
 }
 
+nsContextMenu.prototype.setMessageTargets = function WAT_setMessageTargets (aNode) {
+  var tabmail = document.getElementById("tabmail");
+  if (tabmail) {
+      this.inMessageArea = tabmail.selectedTab.mode.name in mailTabType.modes;
+  } else {
+      this.inMessageArea = true;
+  }
+  if (!this.inMessageArea) {
+      this.inThreadPane = false;
+      this.numSelectedMessages = 1;
+      this.isNewsgroup = false;
+      this.hideMailItems = true;
+      return;
+  }
+  this.inThreadPane = this.popupNodeIsInThreadPane(aNode);
+  this.messagepaneIsBlank = document.getElementById("messagepane").contentWindow.location.href == "about:blank";
+  this.numSelectedMessages = GetNumSelectedMessages();
+  if (this.numSelectedMessages > 0 && Services.prefs.getBoolPref("conversations.enabled")) {
+    this.messagepaneIsBlank = false;
+  }
+  this.isNewsgroup = gFolderDisplay.selectedMessageIsNews;
+  this.hideMailItems = !this.inThreadPane && (this.onImage || this.onLink);
+};
+
